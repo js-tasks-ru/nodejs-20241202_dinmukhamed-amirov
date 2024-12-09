f6facacbafe3a6787210e93d9c0115cb66a37331
Dimash <dinmukhamed.amirov@gmail.com>
2024-12-09 16:44:14 +0500

    feat: add task crud

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -1,17 +1,79 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from "@nestjs/common";
 import { Task } from "./task.model";
+import { TaskStatus } from "./task.model";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task {
+    const taskById = this.tasks.find((task) => task.id == id);
 
-  createTask(task: Task): Task {}
+    if (!taskById) {
+      throw new NotFoundException(`Resource with ID ${id} not found`);
+    }
 
-  updateTask(id: string, update: Task): Task {}
+    return taskById;
+  }
 
-  deleteTask(id: string): Task {}
+  createTask(task: Task): Task {
+    if (typeof task.status != "string" || typeof task.description != "string") {
+      throw new BadRequestException("Not string");
+    }
+
+    if (!Object.values(TaskStatus).includes(task.status)) {
+      throw new BadRequestException("Not right status");
+    }
+
+    const newTask = { id: Math.random().toString(), ...task };
+    this.tasks.push(newTask);
+    return newTask;
+  }
+
+  updateTask(id: string, update: Task): Task {
+    if (
+      typeof update.status != "string" ||
+      typeof update.description != "string"
+    ) {
+      throw new BadRequestException("Not string");
+    }
+
+    if (!Object.values(TaskStatus).includes(update.status)) {
+      throw new BadRequestException("Not right status");
+    }
+
+    const taskIndexById = this.tasks.findIndex((task) => task.id == id);
+
+    if (taskIndexById === -1) {
+      throw new NotFoundException(`Resource with ID ${id} not found`);
+    }
+
+    this.tasks[taskIndexById] = { ...this.tasks[taskIndexById], ...update };
+
+    return this.tasks[taskIndexById];
+  }
+
+  deleteTask(id: string): Task {
+    const taskIndexById = this.tasks.findIndex((task) => task.id == id);
+
+    if (taskIndexById === -1) {
+      throw new NotFoundException(`Resource with ID ${id} not found`);
+    }
+
+    if (taskIndexById === -1) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+
+    const [deletedTask] = this.tasks.splice(taskIndexById, 1);
+
+    return deletedTask;
+  }
 }
