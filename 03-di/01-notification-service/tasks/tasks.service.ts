27903678d8700c609b1c59f15021e9c1ea84fdff
Dimash <dinmukhamed.amirov@gmail.com>
2024-12-25 14:14:03 +0500

    feat: 3 task

--- 03-di/01-notification-service/tasks/tasks.service.ts
+++ 03-di/01-notification-service/tasks/tasks.service.ts
@@ -1,14 +1,24 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateTaskDto, Task, TaskStatus, UpdateTaskDto } from "./task.model";
+import { NotificationService } from "../providers/NotificationService";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private readonly notificationService: NotificationService) {}
 
   async createTask(createTaskDto: CreateTaskDto) {
     const { title, description, assignedTo } = createTaskDto;
+
+    if (title && description && assignedTo) {
+      this.notificationService.sendEmail(
+        "user1@mail.com",
+        "Новая задача",
+        'Вы назначены ответственным за задачу: "New Task"',
+      );
+    }
+
     const task: Task = {
       id: (this.tasks.length + 1).toString(),
       title,
@@ -25,6 +35,13 @@ export class TasksService {
     const task = this.tasks.find((t) => t.id === id);
     if (!task) {
       throw new NotFoundException(`Задача с ID ${id} не найдена`);
+    }
+
+    if (id && updateTaskDto.status === TaskStatus.Completed) {
+      this.notificationService.sendSMS(
+        "+987654321",
+        'Статус задачи "Existing Task" обновлён на "completed"',
+      );
     }
 
     Object.assign(task, updateTaskDto);
