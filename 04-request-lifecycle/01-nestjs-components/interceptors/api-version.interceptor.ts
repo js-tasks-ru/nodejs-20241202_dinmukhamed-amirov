f315570981c524c53f6aecd749ad9197e3e9bd79
Dimash <dinmukhamed.amirov@gmail.com>
2024-12-31 13:27:43 +0500

    feat: add home number 4

--- 04-request-lifecycle/01-nestjs-components/interceptors/api-version.interceptor.ts
+++ 04-request-lifecycle/01-nestjs-components/interceptors/api-version.interceptor.ts
@@ -1,5 +1,17 @@
 import { NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
+import { Observable, map, tap } from "rxjs";
 
 export class ApiVersionInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const start = Date.now();
+    let end = 0;
+    return next.handle().pipe(
+      tap(() => (end = Date.now() - start)),
+      map((response) => ({
+        tasks: response.tasks,
+        apiVersion: "1.0",
+        executionTime: end + "ms",
+      })),
+    );
+  }
 }
