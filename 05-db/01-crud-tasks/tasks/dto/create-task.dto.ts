d1089d7cd3be2593cfa9cf3d0667f36146e25f82
Dimash <dinmukhamed.amirov@gmail.com>
2025-01-07 13:23:20 +0500

    feat: task 5

--- 05-db/01-crud-tasks/tasks/dto/create-task.dto.ts
+++ 05-db/01-crud-tasks/tasks/dto/create-task.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTaskDto {}
+import { IsString, IsNotEmpty, IsOptional, IsBoolean } from "class-validator";
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isCompleted?: boolean; // Необязательное поле с флагом завершенности
+}
--- 05-db/01-crud-tasks/tasks/tasks.controller.ts
+++ 05-db/01-crud-tasks/tasks/tasks.controller.ts
@@ -1,22 +1,44 @@
-import { Controller, Get, Post, Patch, Delete } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  NotFoundException,
+} from "@nestjs/common";
 import { TasksService } from "./tasks.service";
+import { Task } from "./entities/task.entity";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { UpdateTaskDto } from "./dto/update-task.dto";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @Post()
-  create() {}
-
   @Get()
-  findAll() {}
+  findAll(): Promise<Task[]> {
+    return this.tasksService.findAll();
+  }
 
   @Get(":id")
-  findOne() {}
+  findOne(@Param("id") id: number): Promise<Task> {
+    return this.tasksService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() task: CreateTaskDto): Promise<Task> {
+    return this.tasksService.create(task);
+  }
 
   @Patch(":id")
-  update() {}
+  update(@Param("id") id: number, @Body() task: UpdateTaskDto): Promise<Task> {
+    return this.tasksService.update(id, task);
+  }
 
   @Delete(":id")
-  remove() {}
+  remove(@Param("id") id: number): Promise<void> {
+    return this.tasksService.remove(id);
+  }
 }
